906dbb3449f244fedd337a100c05da9f3ac607fa
YKizou <62426491+YKizou@users.noreply.github.com>
2022-02-23 20:02:17 +0100

    Store tasks in Local Storage

--- pages/planner.tsx
+++ pages/planner.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import type { NextPage } from 'next';
 import { IAction, CalendarState, TaskState } from '../types';
 import { getCurrentWeekStart } from '../common/utils';
@@ -10,15 +10,33 @@ import taskMiddleware from "../store/tasks/actions";
 import Layout from '../components/Layout';
 import Calendar from '../components/Calendar';
 
+
 const Planner: NextPage = () => {
     // calendar reducer
     const initialCalendarState: CalendarState = getCurrentWeekStart();
     const calendarMiddlewares = [calendarMiddleware];
     const [calendarStore, calendarDispatch] = useReducerWithMiddleware<CalendarState, IAction>(calendarReducer, initialCalendarState, calendarMiddlewares);
+    
+    //retrieving tasks from local storage
+    var storedTasks: any = ""
+    if (typeof window !== 'undefined') {
+        const saved = localStorage.getItem("tasks");
+        var storedTasks = JSON.parse(saved ? saved : "");
+    } 
 
     // tasks reducer
     const taskMiddlewares = [taskMiddleware];
-    const [taskStore, taskDispatch] = useReducerWithMiddleware<TaskState, IAction>(TaskReducer, {}, taskMiddlewares);
+    const [taskStore, taskDispatch] = useReducerWithMiddleware<TaskState, IAction>(TaskReducer, storedTasks, taskMiddlewares);
+    console.log("ok", taskMiddlewares)
+    console.log("k", taskStore)
+
+
+    // storing tasks in local storage
+    useEffect(() => {
+        const isEmpty = Object.keys(taskStore).length === 0;
+        !isEmpty ? localStorage.setItem("tasks", JSON.stringify(taskStore)) : null ;
+      }, [taskStore]);
+    
 
     return (
         <Layout pageTitle="Calendar" calendarDispatch={calendarDispatch}>
