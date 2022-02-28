6c0d49abe9348473cef9502c518764a94b4266f1
YKizou <62426491+YKizou@users.noreply.github.com>
2022-02-28 17:47:20 +0100

    Login and Signup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,21 @@ import type { NextPage } from 'next'
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 import Planner from './planner';
 import Login from './login';
-
-
+import Register from './register';
 
 const Home: NextPage = () => {
-//   const user = {
-//     "_id": "621bb7377e3b531ca6d5a836",
-//     "username": "youssef",
-//     "email": "youssef@gmail.comm",
-//     "profilePicture": "",
-//     "__v": 0,
-//     "city": "asnieres sur seine"
-// }
-  const user = null
+
+if (typeof window !== 'undefined') {
+  const saved = localStorage.getItem("user");
+  var localUser = JSON.parse(saved ? saved : "{}")}
 
   return (<>
     {typeof window !== 'undefined'? 
     <BrowserRouter>
       <Routes>
-        <Route  path="/" element={user ? <Planner/> : <Login/> }/>
-        <Route  path="/login" element={user ? <Navigate to="/" />: <Login/>}/>
-
+        <Route  path="/" element={Object.keys(localUser).length !== 0 ? <Planner/> : <Login/> }/>
+        <Route  path="/login" element={Object.keys(localUser).length !== 0  ? <Navigate to="/" />: <Login/>}/>
+        <Route  path="/register" element={Object.keys(localUser).length !== 0  ? <Navigate to="/" />: <Register/>}/>
       </Routes>
     </BrowserRouter> : 
     <div></div>}
